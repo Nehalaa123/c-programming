1f736278d0697adc5bfaf1260f0d5ab218c23011
Nehalaa123 <fathimanehala123@gmail.com>
2026-09-24 11:17:02 +0530

    Add files via upload

---
+++ queuearray/output.c
@@ -0,0 +1,70 @@
+Enter size of queue:3
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:1
+Enter the element:10
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:1
+Enter the element:20
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:1
+Enter the element:30
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:1
+Queue is full
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:3
+Queue is:102030
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:2
+Deleted10
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:2
+Deleted20
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:2
+Deleted30
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:3
+Queue is empty
+MENU
+1.Enqueue
+2.Dequeue
+3.Display
+4.exit
+Enter your choice:4
